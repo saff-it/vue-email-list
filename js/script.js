0ce8295941c1481c7fa2980352077ffb1f19c840
saff-it <saff_it@ymail.com>
2022-07-13 16:09:00 +0200

    Exercise 1 - code update

--- js/script.js
+++ js/script.js
@@ -14,27 +14,22 @@ const app = new Vue (
         methods: {
 
             getEmail: function(){
-                
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then( (response) => {
+                for ( let i = 0; i < 10; i++ ){
+                    
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then( (response) => {
                     this.randomEmail.push(response.data.response);
                     console.log(this.randomEmail);
-    
-                });
-
+                    });
+                };
             },
-
-
         },
 
+        created() {
+            this.getEmail();
+        },
         
-        created(){
-            
-            for ( let i = 0; i < 10; i++ ){
-                this.getEmail();
-            };
-        },
-
     },
-
 );
+
+
